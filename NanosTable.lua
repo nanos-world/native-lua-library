f96d808624bb11811db4a1adbf636956b189f8b0
Cheatoid <Cheatoid@users.noreply.github.com>
2026-04-18 22:44:22 +0200

    Remove unused value

--- NanosTable.lua
+++ NanosTable.lua
@@ -37,7 +37,7 @@ function NanosTable.Dump(original_table)
 			-- Stores all keys in another table, sorting it
 			local keys = {}
 
-			for key, v in pairs(object) do
+			for key in pairs(object) do
 				table_insert(keys, key)
 			end
 
