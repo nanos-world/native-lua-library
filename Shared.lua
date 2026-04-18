d90d4b41d9c2e002f74803a1937988c24f3df56b
Stricky <48346353+Stricky4@users.noreply.github.com>
2026-04-18 13:08:38 +0200

    perf: refactor print using string.format for better performance
    Replaced the manual loop/buffer logic with a more efficient one-liner.
    Using string.format and string.rep is significantly faster than manual 
    table insertion in this environment.
    
    Credits to vugi99 (https://github.com/vugi99) for this optimization.

--- Shared.lua
+++ Shared.lua
@@ -1,14 +1,6 @@
 -- Overrides print to call Console Log instead
 print = function(...)
-	--caching
-	local buffer, _tostring, _concat = {...}, tostring, table.concat
-
-	for i = 1, select("#", ...) do
-		buffer[i] = _tostring(buffer[i])
-	end
-
-	-- After all, concatenate the results
-	return Console.Log(_concat(buffer, "\t"))
+	return Console.Log(string.format(string.rep("%s\t", select("#", ...)), ...))
 end
 
 -- Generates a random seed based on the current time for this session
