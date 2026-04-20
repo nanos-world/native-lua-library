10e100aa5b27bb206a45a50db8e7edc045b79cd2
Gabriel T. Nardy (Syed) <gtnardy@gmail.com>
2026-04-20 13:31:28 -0300

    Update Shared.lua
    Removed return as it's not used

--- Shared.lua
+++ Shared.lua
@@ -1,6 +1,6 @@
 -- Overrides print to call Console Log instead
 print = function(...)
-	return Console.Log(string.format(string.rep("%s\t", select("#", ...)), ...))
+	Console.Log(string.format(string.rep("%s\t", select("#", ...)), ...))
 end
 
 -- Generates a random seed based on the current time for this session
